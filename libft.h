171cbaf11af57296fdb1ea860b0b34239808769b
Abdelhak Bensahraoui <ah.bensahraoui@gmail.com>
2022-02-24 19:54:42 +0400

    Day4: ft_strrev.c

--- libft.h
+++ libft.h
@@ -1,8 +1,9 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_min(int a, int b);
-int	ft_max(int a, int b);
+int	    ft_min(int a, int b);
+int 	ft_max(int a, int b);
 long	ft_abs(int n);
+char    *ft_strrev(char *str);
 
 #endif
