e1753ca8ebe90b61bc1e9ccb28132e357eef0d37
Abdelhak Bensahraoui <ah.bensahraoui@gmail.com>
2022-02-28 20:19:43 +0400

    Day8: hb_min_va.c

---
+++ hb_min_va.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+
+int hb_min_va(int va_len, ...)
+{
+    int     min;
+    int     i;
+    int     temp;
+    va_list va;
+
+    i = 0;
+    va_start(va, va_len);
+    min = va_arg(va, int);
+    while (i < va_len)
+    {
+        temp = va_arg(va, int);
+        if (min > temp)
+            min = temp;
+        i++;
+    }
+    va_end(va);
+    return (min);
+}
